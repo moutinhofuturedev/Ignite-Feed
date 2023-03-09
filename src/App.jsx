2c593e01b11be496ac9d3306d82c77734ac81e1e
Paulo Moutinho <paulo_vicali@icloud.com>
2023-03-09 12:43:50 -0300

    chore: inicia repositório

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Post } from "./Post"
+
+function App() {
+  return(
+    <div>
+      <Post author="Paulo" content="Lorem ipsum dolor sit amet, consectetur adip"/>
+      <Post author="John" content="Lorem ipsum dolor"/>
+      <Post author="Nicolas" content="lorem Ipsum dolor"/>
+    </div>
+  )
+}
+
+export default App
