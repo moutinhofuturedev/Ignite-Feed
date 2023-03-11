dd786c66865f865c6053e79d90cb30d820cf0403
Paulo Moutinho <paulo_vicali@icloud.com>
2023-03-11 14:25:39 -0300

    feat(04-develop): adiciona componente sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import { Post } from "./Post"
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar";
+
+import styles from "./styles/App.module.css"
 
 function App() {
-  return(
+  return (
     <div>
       <Header />
-      <Post author="Paulo" content="Lorem ipsum dolor sit amet, consectetur adip"/>
-      <Post author="John" content="Lorem ipsum dolor"/>
-      <Post author="Nicolas" content="lorem Ipsum dolor"/>
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Paulo"
+            content="Lorem ipsum dolor sit amet, consectetur adip"
+          />
+          <Post author="John" content="Lorem ipsum dolor" />
+          <Post author="Nicolas" content="lorem Ipsum dolor" />
+        </main>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
