58a2991588e28424bfa27a5acb47b3f1a16091a0
Paulo Moutinho <paulo_vicali@icloud.com>
2023-03-16 12:39:10 -0300

    feature(06-develop): cria insejos de posts

---
+++ src/components/Post/index.jsx
@@ -0,0 +1,50 @@
+import styles from "./index.module.css"
+import data from "../../../data.json"
+
+export function Post() {
+  return (
+    <>
+      <article className={styles.post}>
+        <header>
+          <div className={styles.author}>
+            <img className={styles.avatar} src={data.avatar} />
+            <div className={styles.authorInfo}>
+              <strong>{data.name}</strong>
+              <span>{data.jobTitle}</span>
+            </div>
+          </div>
+          <time title="13 de Março às 08:52" dateTime="2023-03-13 08:52:30">
+            Publicado há 1H
+          </time>
+        </header>
+        <div className={styles.content}>
+          <p>
+            <p>Fala galeraa 👋</p>
+
+            <p>
+              Acabei de subir mais um projeto no meu portifa. É um projeto que
+              fiz no NLW Return, evento da Rocketseat. O nome do projeto é
+              DoctorCare 🚀
+            </p>
+
+            <p>
+              👉 <a href="">jane.design/doctorcare</a>
+            </p>
+
+            <p>
+              <a href="">#novoprojeto #nlw #rocketseat </a>
+            </p>
+          </p>
+        </div>
+
+        <form className={styles.commentForm}>
+          <strong>Deixe seu comentário</strong>
+          <textarea placeholder="Deixe um comentário"></textarea>
+          <footer>
+            <button type="submit">Publicar</button>
+          </footer>
+        </form>
+      </article>
+    </>
+  );
+}
