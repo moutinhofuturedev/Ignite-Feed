8adc67b8d123c151998837301855624f8b2d0baf
Paulo Moutinho <paulo_vicali@icloud.com>
2023-05-31 13:12:17 -0300

    ci(main): integração com cypress clould

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: 'amj4tt',
   e2e: {
     baseUrl: "https://feed-ruddy.vercel.app/",
-    screenshotOnRunFailure: false,
-    video: false,
-    videoUploadOnPasses: false,
+    screenshotOnRunFailure: true,
+    video: true,
+    videoUploadOnPasses: true,
   },
 });
