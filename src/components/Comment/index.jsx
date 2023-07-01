6045fcc3d7f0958cdd9860394d89663d55ffae87
Paulo Moutinho <paulo_vicali@icloud.com>
2023-07-01 12:38:05 -0300

    feature(develop): cria contagem de likes

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,10 +1,21 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './index.module.css'
 import { Avatar } from '../Avatar'
+import { useState } from 'react'
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0)
+
   const handleDeleteComment = () => {
     onDeleteComment(content)
+  }
+
+  // sempre que atualizar uma informação do valor que tinha anteriormente ( state ),
+  // utilizar este padrão de função abaixo
+  const handleLikeComment = () => {
+    setLikeCount((state) => {
+      return state + 1
+    })
   }
 
   return (
@@ -27,9 +38,9 @@ export function Comment({ content, onDeleteComment }) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
