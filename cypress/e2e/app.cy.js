104689c1bc0d1952dda4900e9a404d174ce22a03
Paulo Moutinho <paulo_vicali@icloud.com>
2023-07-01 12:37:41 -0300

    refactor(develop): remancando testes e2e

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,36 +1,19 @@
+/* eslint-disable no-undef */
 /// <reference types="Cypress" />
-import data from "../../data.json"
-import info from "../fixtures/example.json"
+import data from '../../data.json'
 
-describe("<App />", () => {
-  beforeEach("Visit my app and check element title", () => {
-    cy.visit("/");
+describe('<App />', () => {
+  beforeEach('Visit my app and check element title', () => {
+    cy.visit('/')
 
-    cy.title().should("contain", "Feed");
-  });
+    cy.title().should('contain', 'Feed')
+  })
 
-  it("deve verificar elementos do sidebar", () => {
-    cy.get("strong").contains(data.name);
-    cy.get("span").contains(data.jobTitle);
+  it('deve verificar elementos do sidebar', () => {
+    cy.get('strong').contains(data.name)
+    cy.get('span').contains(data.jobTitle)
 
-    cy.contains("Editar seu perfil").click();
-    cy.url().should("include", "/#");
-  });
-
-  it("deve clicar no campo antes de digitar e mostrar botão de publicar", () => {
-    cy.get("strong").contains("Deixe seu comentário").should("be.visible");
-    cy.validateInfo(info.body);
-  });
-
-  it("deve publicar comentário digitado em campo de texto", () => {
-    cy.validateInfo(info.comments);
-    cy.get('p').contains(info.comments).should("be.visible");
-  });
-
-  it("deve excluir comentário a partir de uma publicação", () => {
-    cy.validateInfo(info.comments);
-    cy.get('p').contains(info.comments).should("be.visible");
-    cy.get("header > button").click();
-    expect(true).to.equal(true);
-  });
-});
+    cy.contains('Editar seu perfil').click()
+    cy.url().should('include', '/#')
+  })
+})
