c83476c8f5e5880f86aed658603cf5d9c020a936
Paulo Moutinho <paulo_vicali@icloud.com>
2023-07-28 01:35:27 -0300

    refactor(remove): remove sonar (#22)

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
   projectId: 'amj4tt',
   e2e: {
-    baseUrl: "https://feed-ruddy.vercel.app/",
+    baseUrl: 'https://feed-ruddy.vercel.app/',
     screenshotOnRunFailure: true,
     video: true,
     videoUploadOnPasses: true,
   },
-});
+})
